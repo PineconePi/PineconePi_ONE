8367004a11978018c7926400b1129993c7b1a5dc
董程森|Chengsen Dong <1034029664@qq.com>
2019-05-27 11:44:14 +0800

    update

--- Library/LCD/Lcd Board/out/startup_swm320.d
+++
@@ -1,1 +0,0 @@
-.\out\startup_swm320.o: ..\..\SWM320_StdPeriph_Driver\CSL\CMSIS\DeviceSupport\startup\arm\startup_SWM320.s
--- Library/LCD/Lcd Board/out/ugui.d
+++
@@ -1,4 +0,0 @@
-.\out\ugui.o: APP\ugui.c
-.\out\ugui.o: APP\ugui.h
-.\out\ugui.o: APP\ugui_config.h
-.\out\ugui.o: E:\MDK5\ARM\ARMCC\Bin\..\include\stdint.h
